b89819a90cd758972f83aa6f505a182d17026c52
Marco Aurélio Santos de Meneses <marcobrmeneses02@outlook.com>
2024-09-04 14:47:18 -0300

    teste, teste e mais teste

--- main.py
+++ main.py
@@ -79,6 +79,10 @@ assistant = client.beta.assistants.create(
             
             
             ## Avalie as redações que foram enviadas e retorne o título da redação, a nota final e a nota de cada competencia.
+              Siga todos os critérios apresentados e faça a correção e explicação de acordo com as pontuações.
+            
+            #Exemplo de redação nota 1000:
+            Em sua canção “Pela Internet”, o cantor brasileiro Gilberto Gil louva a quantidade de informações disponibilizadas pelas plataformas digitais para seus usuários. No entanto, com o avanço de algoritmos e mecanismos de controle de dados desenvolvidos por empresas de aplicativos e redes sociais, essa abundância vem sendo restringida e as notícias, e produtos culturais vêm sendo cada vez mais direcionados – uma conjuntura atual apta a moldar os hábitos e a informatividade dos usuários. Desse modo, tal manipulação do comportamento de usuários pela seleção prévia de dados é inconcebível e merece um olhar mais crítico de enfrentamento. Em primeiro lugar, é válido reconhecer como esse panorama supracitado é capaz de limitar a própria cidadania do indivíduo. Acerca disso, é pertinente trazer o discurso do filósofo Jürgen Habermas, no qual ele conceitua a ação comunicativa: esta consiste na capacidade de uma pessoa em defender seus interesses e demonstrar o que acha melhor para a comunidade, demandando ampla informatividade prévia. Assim, sabendo que a cidadania consiste na luta pelo bem-estar social, caso os sujeitos não possuam um pleno conhecimento da realidade na qual estão inseridos e de como seu próximo pode desfrutar do bem comum – já que suas fontes de informação estão direcionadas –, eles serão incapazes de assumir plena defesa pelo coletivo. Logo, a manipulação do comportamento não pode ser aceita em nome do combate, também, ao individualismo e do zelo pelo bem grupal. Em segundo lugar, vale salientar como o controle de dados pela internet vai de encontro à concepção do indivíduo pós-moderno. Isso porque, de acordo com o filósofo pós-estruturalista Stuart-Hall, o sujeito inserido na pós-modernidade é dotado de múltiplas identidades. Sendo assim, as preferências e ideias das pessoas estão em constante interação, o que pode ser limitado pela prévia seleção de informações, comerciais, produtos, entre outros. Por fim, seria negligente não notar como a tentativa de tais algoritmos de criar universos culturais adequados a um gosto de seu usuário criam uma falsa sensação de livrearbítrio e tolhe os múltiplos interesses e identidades que um sujeito poderia assumir. Portanto, são necessárias medidas capazes de mitigar essa problemática. Para tanto, as instituições escolares são responsáveis pela educação digital e emancipação de seus alunos, com o intuito de deixá-los cientes dos mecanismos utilizados pelas novas tecnologias de comunicação e informação e torná-los mais críticos. Isso pode ser feito pela abordagem da temática, desde o ensino fundamental – uma vez que as gerações estão, cada vez mais cedo, imersas na realidade das novas tecnologias – , de maneira lúdica e adaptada à faixa etária, contando com a capacitação prévia dos professores acerca dos novos meios comunicativos. Por meio, também, de palestras com profissionais das áreas da informática que expliquem como os alunos poderão ampliar seu meio de informações e demonstrem como lidar com tais seletividades, haverá um caminho traçado para uma sociedade emancipada. COMENTÁRIO A participante demonstra excelente domínio da modalidade escrita formal da língua portuguesa, uma vez que a estrutura sintática é excelente e há apenas um desvio: uma vírgula usada de forma equivocada no primeiro parágrafo, em “as notícias, e produtos culturais”. Em relação aos princípios da estruturação do texto dissertativo-argumentativo, percebe-se que a participante apresenta uma tese, o desenvolvimento de justificativas que comprovam essa tese e uma conclusão que encerra a discussão. Ou seja, a participante apresenta excelente domínio do texto dissertativo-argumentativo. Além disso, o tema é abordado de forma completa, demonstrando uma leitura cuidadosa da proposta de redação: logo no primeiro parágrafo, a participante já anuncia a problemática ao apontar que os mecanismos de controle de dados são responsáveis por moldar os hábitos e o grau de informatividade dos usuários. Observa-se também o uso produtivo de repertório sociocultural pertinente à discussão proposta pela participante em mais de um momento do texto: no primeiro parágrafo, ao dizer que, diferentemente do que é cantado por Gilberto Gil, hoje as informações disponibilizadas na internet acabam sendo restringidas devido ao controle de dados; no segundo parágrafo, quando apresenta o conceito de ação comunicativa, de Jürgen Habermas, que é prejudicada devido ao direcionamento de informações, o que também prejudica a característica de múltiplas identidades do indivíduo, proposta por Stuart-Hall e apresentada no terceiro parágrafo. Percebe-se também, ao longo da redação, a presença de um projeto de texto estratégico, com informações, fatos e opiniões relacionados ao tema proposto, desenvolvidos de forma consistente e bem organizados em defesa do ponto de vista. Já no primeiro parágrafo, é apresentado o problema da manipulação do comportamento do usuário pelo controle de dados na internet, que, segundo a participante, além de ser inconcebível, deve ser enfrentada. Ao longo do desenvolvimento, ela mostra como a seleção, por meio de algoritmos, daquilo que é visto pelos usuários interferem em suas vidas, seja por limitar seu papel como cidadão como por limitar o acesso ao conhecimento. Por fim, são apresentadas propostas de intervenção articuladas ao problema apontado pelo participante. Em relação à coesão, encontra-se, nessa redação, um repertório diversificado de recursos coesivos, sem inadequações. Há articulação tanto entre os parágrafos (“Em segundo lugar”, “Portanto”) quanto entre as ideias dentro de um mesmo parágrafo (1º parágrafo: “seus”, “No entanto”, “essa”, “Desse modo”; 2º parágrafo: “Acerca disso”, “assim”, “já que”, “também”; 3º parágrafo: “Isso”, “porque”, “Sendo assim”, “tais”, “Por fim”; 4º parágrafo: “Para tanto”, “seus”, “uma vez que”, entre outros). Por fim, a participante elabora excelente proposta de intervenção, concreta, detalhada e que respeita os direitos humanos: propõe que as escolas tornem seus alunos mais críticos e que os ensinem a lidar com a seletividade gerada pelos algoritmos.
 
             #Exemplo de resposta:
             (Titulo da redação) - nota final: ()
@@ -95,6 +99,8 @@ assistant = client.beta.assistants.create(
 
     """,
   model="gpt-4o-mini",
+  temperature=0,
+  top_p=0,
   tools=[{"type": "file_search"}],
 )
 
@@ -102,7 +108,7 @@ assistant = client.beta.assistants.create(
 vector_store = client.beta.vector_stores.create(name="REDACOES")
  
 # Ready the files for upload to OpenAI
-file_paths = ["redacoes/redacao1.docx","redacoes/redacao2.docx"]
+file_paths = ["redacoes/redacao1.docx"]
 file_streams = [open(path, "rb") for path in file_paths]
  
 # Use the upload and poll SDK helper to upload the files, add them to the vector store,
@@ -120,21 +126,14 @@ assistant = client.beta.assistants.update(
   tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
 )
 
-# Upload the user provided file to OpenAI
-message_file = client.files.create(
-  file=open("redacoes/redacao1.docx", "rb"), purpose="assistants"
-)
  
 # Create a thread and attach the file to the message
 thread = client.beta.threads.create(
   messages=[
     {
       "role": "user",
-      "content": "Avalie as redações enviadas.",
-      # Attach the new file to the message.
-      "attachments": [
-        { "file_id": message_file.id, "tools": [{"type": "file_search"}] }
-      ],
+      "content": "Faça a correção de todas as redações!",
+      
     }
   ]
 )
